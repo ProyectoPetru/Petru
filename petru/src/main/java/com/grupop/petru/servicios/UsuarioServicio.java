058c8adbb0ac688d474c5d4b463fe41d63bbaaa4
NahiDenice <nahiara.alegre@hotmail.com>
2023-11-22 21:48:19 -0300

    Agegados servicios en UsuarioServicio

--- petru/src/main/java/com/grupop/petru/servicios/UsuarioServicio.java
+++ petru/src/main/java/com/grupop/petru/servicios/UsuarioServicio.java
@@ -16,6 +16,8 @@ import com.grupop.petru.excepciones.MiException;
 import com.grupop.petru.repositorios.UsuarioRepositorio;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -32,8 +34,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class UsuarioServicio implements UserDetailsService{
-    
+public class UsuarioServicio implements UserDetailsService {
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
 
@@ -41,7 +43,8 @@ public class UsuarioServicio implements UserDetailsService{
     private ImagenServicio imagenServicio;
 
     @Transactional
-    public void registrar(MultipartFile archivo, String nombre, String email, String clave, String clave2, Long telefono, String descripcion) throws MiException {
+    public void registrarUsuario(MultipartFile archivo, String nombre, String email, String clave, String clave2,
+            Long telefono, String descripcion) throws MiException {
         validar(nombre, email, clave, clave2, telefono);
         Usuario usuario = new Usuario();
         usuario.setNombre(nombre);
@@ -53,6 +56,71 @@ public class UsuarioServicio implements UserDetailsService{
         usuario.setImagen(imagen);
         usuarioRepositorio.save(usuario);
     }
+
+
+
+    // MODIFICAR USUARIO
+    // El rol y la baja se levantan al encontrar el usuario y se dejan igual en este
+    // método
+    @Transactional
+    public void modificarUsuario(MultipartFile archivo, String idUsuario, String nombre, String email, String clave,
+            String clave2,
+            Long telefono, String descripcion) throws MiException {
+        validar(nombre, email, clave, clave2, telefono);
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+            usuario.setNombre(nombre);
+            usuario.setEmail(email);
+            usuario.setClave(new BCryptPasswordEncoder().encode(clave));
+            Imagen imagen = imagenServicio.guardar(archivo);
+            usuario.setImagen(imagen);
+            usuarioRepositorio.save(usuario);
+        }
+    }
+
+
+
+    // ALTA Y BAJA DE USUARIO
+    @Transactional
+    public void altaDeUsuario(String idUsuario) throws MiException {
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+            usuario.setBaja(false);
+            usuarioRepositorio.save(usuario);
+        }
+
+    }
+
+    @Transactional
+    public void bajaDeUsuario(String idUsuario) throws MiException {
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+            usuario.setBaja(true);
+            usuarioRepositorio.save(usuario);
+        }
+
+    }
+
+
+
+    // MODIFICAR ROL
+
+    @Transactional
+    public void modificarRolUsuario(String idUsuario, Rol rol) {
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+            usuario.setRol(rol);
+            usuarioRepositorio.save(usuario);
+        }
+    }
+
+
+    
+    // VALIDACIONES
 
     private void validar(String nombre, String email, String clave, String clave2, Long telefono) throws MiException {
         if (nombre.isEmpty() || nombre == null) {
@@ -88,5 +156,4 @@ public class UsuarioServicio implements UserDetailsService{
         }
     }
 
-    
 }
