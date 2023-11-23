30704152193e2c490dc790c7def096bac8c85f8d
NahiDenice <nahiara.alegre@hotmail.com>
2023-11-22 21:53:06 -0300

    Arreglado error de nombre de método en PortalControlador

--- petru/src/main/java/com/grupop/petru/controladores/PortalControlador.java
+++ petru/src/main/java/com/grupop/petru/controladores/PortalControlador.java
@@ -42,7 +42,7 @@ public class PortalControlador {
     public String registro(@RequestParam String nombre, @RequestParam String email, @RequestParam String clave,
            @RequestParam String clave2, MultipartFile archivo, @RequestParam Long telefono, String descripcion, ModelMap modelo) {
         try {
-            usuarioServicio.registrar(archivo, nombre, email, clave, clave2, telefono, descripcion);
+            usuarioServicio.registrarUsuario(archivo, nombre, email, clave, clave2, telefono, descripcion);
             modelo.put("exito", "Usuario registrado correctamente!");
             return "index.html";
         } catch (MiException ex) {
