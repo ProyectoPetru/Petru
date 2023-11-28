433a02c66106145dae2187eae4e95c145b52b95b
NahiDenice <nahiara.alegre@hotmail.com>
2023-11-28 19:16:38 -0300

    Un commit para que se haga el pull

--- petru/target/classes/static/js/alertas.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#alertas").querySelector(".error").querySelector(".salir").addEventListener("click", (e) => {
-    const error = document.querySelector("#alertas").querySelector(".error")
-    error.classList.add("error-hide")
-    setTimeout(() => {
-        error.classList.remove("error-hide")
-        error.hidden = true
-        console.log("hola");
-    }, 800)
-})
--- petru/target/classes/static/js/index.js
+++
@@ -1,8 +0,0 @@
-const dataTheme = localStorage.getItem("data-theme")
-const html = document.querySelector("html")
-
-if (dataTheme != null) {
-    html.setAttribute("data-theme", dataTheme)
-} else {
-    html.setAttribute("data-theme", "light")
-}
