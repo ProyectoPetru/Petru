cb9a27f8cc28d85801ab51a4de088170925e06e2
Dominiciss <dominichmartinezmanuel@gmail.com>
2023-12-04 15:56:20 -0300

    Rework de inicio.html (WIP)

--- petru/target/classes/static/js/inicio.js
+++ petru/target/classes/static/js/inicio.js
@@ -1,16 +1,36 @@
-const handleClick = (e) => {
-    const informacion = e.target.parentNode.querySelector(".informacion")
+const carousel = document.querySelector("#inicio").querySelector(".servicios").querySelector(".carousel")
+const totalSlides = carousel.querySelectorAll(".carousel-item").length
+let currentIndex = 0
 
-    if (informacion.hidden) {
-        informacion.classList.remove("informacion-hide")
-        informacion.hidden = false
+const inicioHandleSlide = (index) => {
+    if (index < 0) {
+        currentIndex = totalSlides - 1
+    } else if (index >= totalSlides) {
+        currentIndex = 0
     } else {
-        informacion.classList.add("informacion-hide")
-        setTimeout(() => {
-            if (informacion.classList.contains("informacion-hide")) {
-                informacion.classList.remove("informacion-hide")
-                informacion.hidden = true
-            }
-        }, 400)
+        currentIndex = index
     }
+
+    const translateValue = (-currentIndex * 100) + "%"
+    carousel.querySelector(".carousel-wrapper").style.transform = "translateX(" + translateValue + ")"
 }
+
+const inicioNextSlide = () => {
+    clearInterval(interval)
+    inicioHandleSlide(currentIndex + 1)
+    interval = setInterval(() => {
+        inicioNextSlide()
+    }, 10000)
+}
+
+const inicioPrevSlide = () => {
+    clearInterval(interval)
+    inicioHandleSlide(currentIndex - 1)
+    interval = setInterval(() => {
+        inicioNextSlide()
+    }, 10000)
+}
+
+let interval = setInterval(() => {
+    inicioNextSlide()
+}, 10000)
